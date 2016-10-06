f6619627d2cd874e19be840c9ac59d9eb1353b79
Brandon Tardio <btardio@gmail.com>
2016-10-06 07:08:04 -0700

    Add files via upload

---
+++ app_images/models.py
@@ -0,0 +1,42 @@
+#
+#    
+#
+#    Copyright (C) 2016 Brandon C Tardio
+#
+#    This file is part of DPWC
+#
+#    DPWC is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    DPWC is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with DPWC.  If not, see <http://www.gnu.org/licenses/>.
+#    
+#    Contact: BTardio@gmail.com
+#             818 424 6838
+#
+#
+#    Django_Personal_Website_Concept ( DPWC )
+#
+#
+#
+
+from django.db import models
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.fields import ContentType
+
+
+
+# Create your models here.
+
+
+class images ( models.Model ):
+  
+  image = models.ImageField ( )
+  
